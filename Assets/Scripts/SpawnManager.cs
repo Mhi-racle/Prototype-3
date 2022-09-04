d0e8d81185c4e3a7c5e48f2fab13abc9795715b9
Mhiracle <mhiracleebenezer@gmail.com>
2022-09-04 19:24:56 +0100

    Changing textures and randomizing obtsacles spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2f;
     private float repeatDelay = 2f;
-    public PlayerController playerController;
+    private PlayerController playerController;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +18,8 @@ public class SpawnManager : MonoBehaviour
     {
         if (playerController.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            int random = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[random], spawnPos, obstaclePrefabs[random].transform.rotation);
         }
 
     }
